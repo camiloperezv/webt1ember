3c7567a43e77e4049ad8654f22a92939a2dfb4c9
daniel molina <programanime@gmail.com>
2017-05-06 13:26:01 -0500

    guardado horario doctor

--- app/routes/doctors/schedule.js
+++ app/routes/doctors/schedule.js
@@ -1,7 +1,27 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
+    ajax: Ember.inject.service(),
+    doctor:{},
     model(params){
-        
+        let that=this;
+        return new RSVP.Promise(function(resolve) {
+            that.get('ajax').request('/api/v1/doctors').then(function(data){
+                let doctors=data;
+                console.log(data);
+                for(var i=0;i<doctors.length;i++){
+                    if(doctors[i].docId==params.doctor_id)
+                        that.doctor=doctors[i];
+                    }
+
+                resolve(that.doctor);
+            });
+        });
+    },
+    actions:{
+        fijarHorario(){
+            alert("guardado exitoso"+this.doctor.init);
+        }
     }
 });
